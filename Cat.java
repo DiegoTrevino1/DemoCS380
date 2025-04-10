eab14d89da47029423988b14d3de6758790dd682
Diego <trevinodi@cwu.edu>
2025-04-10 13:25:02 -0700

    Add 1st Cat.java

---
+++ Cat.java
@@ -0,0 +1,18 @@
+public class TestSuite {
+	public static void main(String[] args) {
+	System.out.println("Hello, Cat!");
+	System.out.println("Hello, Dog!");
+
+	}
+}
+
+
+public class Cat{
+	private String name;
+	private int age;
+	//This is a cat function
+	public Cat(String name, int age(){
+		this.age = age;
+		this.name = name;
+	}
+}
