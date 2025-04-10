6cb7bf10bf4f1f453c3313896a71743ee5fec3cd
Diego <trevinodi@cwu.edu>
2025-04-10 13:30:58 -0700

    Updated Dog.java with CS380

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class TestSuite {
+	public static void main(String[] args) {
+		System.out.println("Hello, Dog!");
+		System.out.println("Hello, CS380");
+
+	}
+}
+
+public class Dog{
+	private String name;
+
+}
+
+public String getName(){
+	return name;
+}
